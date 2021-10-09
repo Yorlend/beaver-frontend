bf59bb2bcfcd46227e4a18e9698ded892d244e12
Rull Deef <deeroll666@gmail.com>
2021-10-09 15:29:44 +0300

    extract header & footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,146 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 export default function Footer() {
-    return <footer className="position-absolute">
-        <Container>
-            <p className="m-0 py-3 text-white">&copy; 2021 Web Beavers</p>
-        </Container>
+    return <footer className="footer">
+        <div className="footer-main">
+            <div className="container">
+                <div className="row items">
+                    <div className="col-xl-3 col-lg-3 col-md-5 col-12 item">
+                        {/* Begin company info */}
+                        <div className="footer-company-info">
+                            <div className="footer-company-top">
+                                <a href="/" className="logo" title="PathSoft">
+                                    <img data-src="assets/img/logo-white.svg" className="lazy" src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" alt="PathSoft" width={115} height={36} />
+                                </a>
+                                <div className="footer-company-desc">
+                                    <p>Our company has been developing high-quality and reliable software for
+                                        corporate needs since 2008. We are renowned professionals of software
+                                        development.</p>
+                                </div>
+                            </div>
+                            <ul className="footer-social-links">
+                                <li>
+                                    <a href="#!" title="Facebook">
+                                        <svg viewBox="0 0 320 512">
+                                            <use xlinkHref="assets/img/sprite.svg#facebook-icon" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#!" title="Instagram">
+                                        <svg viewBox="0 0 448 512">
+                                            <use xlinkHref="assets/img/sprite.svg#instagram-icon" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#!" title="LinkedIn">
+                                        <svg viewBox="0 0 448 512">
+                                            <use xlinkHref="assets/img/sprite.svg#linkedin-icon" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#!" title="Twitter">
+                                        <svg viewBox="0 0 512 512">
+                                            <use xlinkHref="assets/img/sprite.svg#twitter-icon" />
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* End company info */}
+                    </div>
+                    <div className="col-xl-2 offset-xl-1 col-md-3 col-5 col-lg-2 item">
+                        <div className="footer-item">
+                            <p className="footer-item-heading">Menu</p>
+                            <nav className="footer-nav">
+                                <ul className="footer-mnu">
+                                    <li><a href="about-us.html" className="hover-link" data-title="About Us"><span>About
+                                        Us</span></a></li>
+                                    <li><a href="services.html" className="hover-link" data-title="Services"><span>Services</span></a></li>
+                                    <li><a href="typography.html" data-title="Typography"><span>Typography</span></a></li>
+                                    <li><a href="news.html" className="hover-link" data-title="News"><span>News</span></a></li>
+                                    <li><a href="contact-us.html" className="hover-link" data-title="Contact Us"><span>Contact Us</span></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div className="col-xl-2 col-lg-3 col-md-3 col-7 item">
+                        <div className="footer-item">
+                            <p className="footer-item-heading">What We Offer</p>
+                            <nav className="footer-nav">
+                                <ul className="footer-mnu">
+                                    <li><a href="#!" className="hover-link" data-title="DB Management"><span>DB
+                                        Management</span></a></li>
+                                    <li><a href="#!" className="hover-link" data-title="IOS/MacOS"><span>IOS/MacOS</span></a></li>
+                                    <li><a href="#!" className="hover-link" data-title="Android Apps"><span>Android
+                                        Apps</span></a></li>
+                                    <li><a href="#!" className="hover-link" data-title="Windows Apps"><span>Windows
+                                        Apps</span></a></li>
+                                    <li><a href="#!" className="hover-link" data-title="UX & UI"><span>UX &amp;
+                                        UI</span></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div className="col-xs-4 col-lg-4 col-12 item">
+                        <div className="footer-item">
+                            <p className="footer-item-heading">Our contacts</p>
+                            <ul className="footer-contacts">
+                                <li>
+                                    <i className="material-icons md-22">location_on</i>
+                                    <div className="footer-contact-info">
+                                        301 S Irving Blvd Los Angeles, CA 90020
+                                    </div>
+                                </li>
+                                <li>
+                                    <i className="material-icons md-22 footer-contact-tel">smartphone</i>
+                                    <div className="footer-contact-info">
+                                        <a href="#!" className="formingHrefTel">+1 323-913-4688</a>, <a href="#!" className="formingHrefTel">+1 323-888-4554</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i className="material-icons md-22 footer-contact-email">email</i>
+                                    <div className="footer-contact-info">
+                                        <a href="mailto:mail@example.com">mail@example.com</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="footer-item">
+                            <p className="footer-item-heading">Subscribe</p>
+                            <form action="#!" method="post" className="footer-subscribe">
+                                <div className="form-field">
+                                    <label htmlFor="subscribe-email" className="form-field-label">Your email</label>
+                                    <input type="email" className="form-field-input" name="Subscribe_email" defaultValue autoComplete="off" required="required" id="subscribe-email" />
+                                </div>
+                                <div className="form-btn">
+                                    <button type="submit" className="btn ripple"><span>Subscribe</span></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="footer-bottom">
+            <div className="container">
+                <div className="row justify-content-between items">
+                    <div className="col-md-auto col-12 item">
+                        <nav className="footer-links">
+                            <ul>
+                                <li><a href="terms-and-conditions.html">Terms and Conditions</a></li>
+                                <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-md-auto col-12 item">
+                        <div className="copyright">© 2021 PathSoft. All rights reserved.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,211 @@
-import React, {Component} from 'react';
-import {Container, Navbar, Nav} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import React, { Component } from 'react';
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
     render() {
         return (
-            <Navbar sticky="top" collapseOnSelect expand="md" bg="white" variant="light">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            src="/icon.png"
-                            height="30"
-                            width="30"
-                            className="d-inline-block align-top"
-                            alt="Logo"
-                        /> Web Beavers
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Link to="/photosets" className="py-2 py-md-0 px-md-4 text-dark">Датасеты</Link>
-                            <Link to="/devices" className="py-2 py-md-0 px-md-4 text-dark">Устройства</Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <div>
+                {/* Begin mobile main menu */}
+                <nav className="mmm">
+                    <div className="mmm-content">
+                        <ul className="mmm-list">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about-us.html">About us</a></li>
+                            <li><a href="services.html">Services</a></li>
+                            <li><a href="typography.html">Typography</a></li>
+                            <li><a href="tabs-and-accordions.html">Tabs &amp; Accordions</a></li>
+                            <li><a href="news.html">News</a></li>
+                            <li><a href="contact-us.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div className="mmm-footer">
+                        <ul className="mmm-lang">
+                            <li className="active"><a href="#!"><span>En</span></a></li>
+                            <li><a href="#!"><span>Sp</span></a></li>
+                            <li><a href="#!"><span>It</span></a></li>
+                            <li><a href="#!"><span>Uk</span></a></li>
+                            <li><a href="#!"><span>Ru</span></a></li>
+                        </ul>
+                    </div>
+                </nav> {/* End mobile main menu */}
+                {/* Begin header */}
+                <header className="header">
+                    {/* Begin header top */}
+                    <nav className="header-top">
+                        <div className="container">
+                            <div className="row align-items-center justify-content-between">
+                                <div className="col-auto">
+                                    {/* Begin header top info */}
+                                    <ul className="header-top-info">
+                                        <li>
+                                            <a href="mailto:mail@example.com">
+                                                <i className="material-icons md-18">mail_outline</i>
+                                                <span>mail@example.com</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="mailto:mail@example.com" className="formingHrefTel">
+                                                <i className="material-icons md-18">phone_in_talk</i>
+                                                <span>+1-888-777-1234</span>
+                                            </a>
+                                        </li>
+                                    </ul>{/* Ennd header top info */}
+                                </div>
+                                <div className="col-auto">
+                                    <div className="header-top-links">
+                                        {/* Begin social links */}
+                                        <ul className="social-links">
+                                            <li>
+                                                <a href="#!" title="Facebook">
+                                                    <svg viewBox="0 0 320 512">
+                                                        <use xlinkHref="assets/img/sprite.svg#facebook-icon" />
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#!" title="Instagram">
+                                                    <svg viewBox="0 0 448 512">
+                                                        <use xlinkHref="assets/img/sprite.svg#instagram-icon" />
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#!" title="LinkedIn">
+                                                    <svg viewBox="0 0 448 512">
+                                                        <use xlinkHref="assets/img/sprite.svg#linkedin-icon" />
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#!" title="Twitter">
+                                                    <svg viewBox="0 0 512 512">
+                                                        <use xlinkHref="assets/img/sprite.svg#twitter-icon" />
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                        </ul>{/* End social links */}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>{/* End header top */}
+                    {/* Begin header fixed */}
+                    <nav className="header-fixed">
+                        <div className="container">
+                            <div className="row flex-nowrap align-items-center justify-content-between">
+                                <div className="col-auto d-block d-lg-none header-fixed-col">
+                                    <div className="main-mnu-btn">
+                                        <span className="bar bar-1" />
+                                        <span className="bar bar-2" />
+                                        <span className="bar bar-3" />
+                                        <span className="bar bar-4" />
+                                    </div>
+                                </div>
+                                <div className="col-auto header-fixed-col">
+                                    {/* Begin logo */}
+                                    <a href="/" className="logo" title="PathSoft">
+                                        <img src="assets/img/logo.svg" alt="PathSoft" width={115} height={36} />
+                                    </a>{/* End logo */}
+                                </div>
+                                <div className="col-auto header-fixed-col d-none d-lg-block col-static">
+                                    {/* Begin main menu */}
+                                    <nav className="main-mnu">
+                                        <ul className="main-mnu-list">
+                                            <li><a href="index.html" data-title="Home"><span>Home</span></a></li>
+                                            <li><a href="about-us.html" data-title="About us"><span>About us</span></a></li>
+                                            <li><a href="services.html" data-title="Services"><span>Services</span></a></li>
+                                            <li><a href="typography.html" data-title="Typography"><span>Typography</span></a></li>
+                                            <li><a href="tabs-and-accordions.html" data-title="Tabs & Accordions"><span>Tabs
+                                                &amp; Accordions</span></a></li>
+                                            <li><a href="news.html" data-title="News"><span>News</span></a></li>
+                                            <li><a href="contact-us.html" data-title="Contact Us"><span>Contact
+                                                Us</span></a></li>
+                                        </ul>
+                                    </nav>{/* End main menu */}
+                                </div>
+                                <div className="col-auto header-fixed-col col-static">
+                                    {/* Begin header actions */}
+                                    <ul className="header-actions">
+                                        {/* Begin header languarge */}
+                                        <li className="d-none d-lg-block">
+                                            <div className="header-lang">
+                                                <div className="header-lang-current"><i className="material-icons md-22">language</i></div>
+                                                <ul className="header-lang-list">
+                                                    <li className="active"><a href="#!" className="hover-link" data-lang="En" data-title="English"><span>English</span></a></li>
+                                                    <li><a href="#!" className="hover-link" data-lang="Sp" data-title="Spanish"><span>Spanish</span></a></li>
+                                                    <li><a href="#!" className="hover-link" data-lang="It" data-title="Italian"><span>Italian</span></a></li>
+                                                    <li><a href="#!" className="hover-link" data-lang="Uk" data-title="Ukraine"><span>Ukraine</span></a></li>
+                                                    <li><a href="#!" className="hover-link" data-lang="Ru" data-title="Russian"><span>Russian</span></a></li>
+                                                </ul>
+                                            </div>
+                                        </li>{/* End header languarge */}
+                                        {/* Begin header navbar */}
+                                        <li className="d-block d-lg-none">
+                                            <div className="header-navbar">
+                                                <div className="header-navbar-btn">
+                                                    <i className="material-icons md-24">more_vert</i>
+                                                </div>
+                                                <ul className="header-navbar-content">
+                                                    <li>
+                                                        <a href="mailto:mail@example.com">
+                                                            <i className="material-icons md-20">mail_outline</i>
+                                                            <span>mail@example.com</span>
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="mailto:mail@example.com" className="formingHrefTel">
+                                                            <i className="material-icons md-20">support_agent</i>
+                                                            <span>1-888-777-1234</span>
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        {/* Begin social links */}
+                                                        <ul className="social-links">
+                                                            <li>
+                                                                <a href="#!" title="Facebook">
+                                                                    <svg viewBox="0 0 320 512">
+                                                                        <use xlinkHref="assets/img/sprite.svg#facebook-icon">
+                                                                        </use>
+                                                                    </svg>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a href="#!" title="Instagram">
+                                                                    <svg viewBox="0 0 448 512">
+                                                                        <use xlinkHref="assets/img/sprite.svg#instagram-icon">
+                                                                        </use>
+                                                                    </svg>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a href="#!" title="LinkedIn">
+                                                                    <svg viewBox="0 0 448 512">
+                                                                        <use xlinkHref="assets/img/sprite.svg#linkedin-icon">
+                                                                        </use>
+                                                                    </svg>
+                                                                </a>
+                                                            </li>
+                                                            <li>
+                                                                <a href="#!" title="Twitter">
+                                                                    <svg viewBox="0 0 512 512">
+                                                                        <use xlinkHref="assets/img/sprite.svg#twitter-icon">
+                                                                        </use>
+                                                                    </svg>
+                                                                </a>
+                                                            </li>
+                                                        </ul>{/* End social links */}
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </li>{/* End header navbar */}
+                                    </ul>{/* End header actions */}
+                                </div>
+                            </div>
+                        </div>
+                    </nav>{/* End header fixed */}
+                </header>{/* End header */}
+            </div>
         );
     }
 }
